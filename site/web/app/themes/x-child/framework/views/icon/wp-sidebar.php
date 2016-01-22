4ac1f2524ec0351963835441a507845e31f54254
Vicens Fayos <datracka@gmail.com>
2016-01-22 17:38:47 +0100

    new version Wordpress and wp-sidebar.php problem

--- site/web/app/themes/x-child/framework/views/icon/wp-sidebar.php
+++ site/web/app/themes/x-child/framework/views/icon/wp-sidebar.php
@@ -16,10 +16,10 @@
                 <?php
                 /*select sidebar on category */
                 $categories = get_the_category();
-                if ($categories[0]->name == "Libros") {
+                if ($categories[0]->name == "libros") {
                     get_sidebar('blog');
                 } else  {
-                    get_sidebar('Libros');
+                    get_sidebar('libros');
                 }
                 ?>
             <?php else : ?>
